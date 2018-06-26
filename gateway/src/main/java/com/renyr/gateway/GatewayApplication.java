a30a3dea4094ee09e8217b0e05b282d9b65b01c9
wocaishiryr <wocaishiryr@163.com>
2018-06-26 18:04:25 +0800

    hystrix

--- gateway/src/main/java/com/renyr/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/renyr/gateway/GatewayApplication.java
@@ -7,28 +7,18 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@RestController
+@EnableHystrix
+@EnableHystrixDashboard
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
-	}
-
-	@GetMapping("/actuator/health")
-	public String health() {
-		return "ok";
 	}
 
 	@Bean
@@ -36,15 +26,6 @@ public class GatewayApplication {
 		return new DiscoveryClientRouteDefinitionLocator(discoveryClient,properties);
 	}
 
-	/*@Bean
-	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-		ZonedDateTime datetime = LocalDateTime.now().plusDays(1).atZone(ZoneId.systemDefault());
-		//@formatter:off
-		return builder.routes()
-				.route("before_route", r -> r.before(datetime)
-						.uri("http://xujin.org"))
-				.build();
-		//@formatter:on
-	}*/
+
 
 }
